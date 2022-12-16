68eabd18033dd752a08488313c0e1776084f99d5
Aboyeji Emmanuel <110109212+fullstackdaddy@users.noreply.github.com>
2022-12-16 02:02:38 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (n > 0)
+		{
+			_putchar(95);
+			i += 1;
+		}
+		else
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
