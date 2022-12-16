ec4cd4780f417a0ad8765bfd2ef8dbde9ef2f72e
Aboyeji Emmanuel <110109212+fullstackdaddy@users.noreply.github.com>
2022-12-16 02:01:42 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
