91728027ecfb21af5a8b5b746fb2f99edde7e82b
Aboyeji Emmanuel <110109212+fullstackdaddy@users.noreply.github.com>
2022-12-23 03:38:01 +0100

    Update 0-strat.c

--- 0x06-pointers_arrays_strings/0-strat.c
+++ 0x06-pointers_arrays_strings/0-strat.c
@@ -1,1 +1,17 @@
-hello
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	return (dest);
+}
