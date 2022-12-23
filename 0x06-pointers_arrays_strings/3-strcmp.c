3978c7097f720df860d950d00c7ee2e94d2c7a58
Aboyeji Emmanuel <110109212+fullstackdaddy@users.noreply.github.com>
2022-12-23 03:44:19 +0100

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int aux;
+
+	while (*(s1 + i) != '\0')
+	{
+		if (*(s1 + i) > *(s2 + i))
+		{
+			aux = *(s1 + i) - *(s2 + i);
+			return (aux);
+		}
+		else if (*(s1 + i) < *(s2 + i))
+		{
+			aux = *(s1 + i) - *(s2 + i);
+			return (aux);
+		}
+		i += 1;
+	}
+	return (0);
+}
