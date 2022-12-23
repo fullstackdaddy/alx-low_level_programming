331c1c02177c990c94403d5fce1d948b0a0d262f
Aboyeji Emmanuel <110109212+fullstackdaddy@users.noreply.github.com>
2022-12-23 03:47:17 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = 0;
+	int *init, *final;
+	int aux = 0;
+
+	init = a;
+	final = a;
+	n -= 1;
+	while (i < n)
+		i += 1;
+	while (j <= i)
+	{
+		aux = *(init + j);
+		*(init + j) = *(final + i);
+		*(final + i) = aux;
+		j++;
+		i--;
+	}
+}
